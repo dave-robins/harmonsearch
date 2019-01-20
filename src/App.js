ef5f3d892470c5f3d665b9f2354a24f72bee01e4
David Robins <dave.a.robins@gmail.com>
2019-01-20 16:06:51 -0600

    fetching xml feed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar'
+// const xml = require("xml-parse");
+
+fetch('https://feeds.megaphone.fm/harmontown')
+      .then(response => response.text())
+      .then(data => console.log(/*xml.parse*/(data)))
 
 class App extends Component {
   render() {
