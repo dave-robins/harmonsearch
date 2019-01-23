48bb8295ddf8cfa8f9bec6f8359e1d98037be8ab
Dave Robins <dave.a.robins@gmail.com>
2019-01-22 19:16:11 -0600

    fixed switch bug, added provider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar'
 import Index from './components/layout/Index'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-
 import convert from 'xml-js';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './App.css'
 
 fetch('https://feeds.megaphone.fm/harmontown')
       .then(response => response.text())
       .then(function(data){
         console.log(convert.xml2json(data, {compact: true, spaces: 2}));
       })
-
-
 
 class App extends Component {
   render() {
@@ -21,12 +19,12 @@ class App extends Component {
           <Navbar/>
           <div className="container">
             <Switch>
-              <Route exact path="/" Component={Index}/>
+              <Route exact path="/" component={Index}/>
             </Switch>
           </div>
         </React.Fragment>
       </Router>
-    );
+    )
   }
 }
 
--- src/components/layout/Index.js
+++ src/components/layout/Index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Episodes from '../episodes/Episodes'
 
 const Index = () => {
-    return(
+    return (
         <React.Fragment>
-            <h1> hello </h1>
-            <Episodes/>
+            <h1>and now the index</h1>
+            <Episodes />
         </React.Fragment>
     )
 }
