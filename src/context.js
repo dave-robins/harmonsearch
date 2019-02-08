390fe604abac59ad4f1da95fa9fd565e973ec37d
Dave Robins <dave.a.robins@gmail.com>
2019-02-07 22:50:21 -0600

    fetched xml and parsed to an array of episode objects

--- src/context.js
+++ src/context.js
@@ -3,41 +3,72 @@ import convert from 'xml-js';
 
 const Context = React.createContext()
 
-const episode = {
-  title: "",
-  description: "",
-  pubDate: "",
-  duration: "",
-  url: ""
+const formatEpisodes = (json => {
+  const episodes = []
+  const filtered = (json.elements[0].elements[0].elements).filter((episode) => episode.name === "item")
+  filtered.map(ep => {
+    const episode = {
+      title: getParam(ep.elements, "title"),
+      description: trimDescription(getParam(ep.elements, "description")),
+      pubDate: getParam(ep.elements, "pubDate"),
+      duration: getParam(ep.elements, "itunes:duration"),
+      url: getLink(ep.elements)
+    }
+    episodes.push(episode)
+    return episode
+  })
+  console.log(episodes)
+  return episodes
+})
+
+const getParam = (item, param) => {
+  const idObject = item.filter((element) => {
+      return element.name === param
+  })
+  return idObject[0].elements[0].text
 }
-/* map over episode list, unwrap each "item", set to episode, 
-* append to new array, pass array into state
-*/
+
+const getLink = (item) => {
+  const urlObject = item.filter((element) => {
+    return element.name === "enclosure"
+  })
+  return urlObject[0].attributes.url
+}
+
+const trimDescription = (text) => {
+  const arr = text.replace("<br>", "").split("<br>")
+  return arr[0]
+}
 
 export class Provider extends Component {
-    state = {
-        episode_list: [],
-        heading: 'Recent Episodes'
-    }
+  state = {
+      episode_list: [],
+      masterList: [],
+      heading: 'Recent Episodes'
+  }
 
-componentDidMount(){
-    fetch('https://feeds.megaphone.fm/harmontown')
-      .then(res => res.text())
-      .then(data => {
-        var newJson = convert.xml2js(data);
-        let unwrapped = (newJson.elements[0].elements[0].elements)
-        let filtered = unwrapped.filter((episode) => episode.name === "item")
-        this.setState({episode_list: filtered})
-      })
-}
+  componentDidMount(){
+      fetch('https://feeds.megaphone.fm/harmontown')
+        .then(res => res.text())
+        .then(data => {
+          var json = convert.xml2js(data);
+          const episodeArray = formatEpisodes(json)
+          let unwrapped = (json.elements[0].elements[0].elements)
+          let filtered = unwrapped.filter((episode) => episode.name === "item")
+          this.setState({
+            episode_list: filtered,
+            masterList: episodeArray
+          })
+        })
+  }
 
-    render() {
-        return (
-            <Context.Provider value={this.state}>
-                {this.props.children}
-            </Context.Provider>
-        )
-    }
+  render() {
+      return (
+          <Context.Provider value={this.state}>
+              {this.props.children}
+          </Context.Provider>
+      )
+  }
 }
 
 export const Consumer = Context.Consumer
