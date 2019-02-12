9b5b194b8a0627986927f72293256de0dd3afa62
Dave Robins <dave.a.robins@gmail.com>
2019-02-12 00:06:54 -0600

    lose the submit button, search on state change, got the episodelist from context

--- src/components/episodes/Search.js
+++ src/components/episodes/Search.js
@@ -1,30 +1,45 @@
 import React, { Component } from 'react'
 import { Consumer } from '../../context'
 
+let heresTheList = []
+
 class Search extends Component {
   state = {
-    trackTitle: ''
+    trackTitle: '',
+    searchList: []
+  }
+
+  search = () => {
+    const arr = heresTheList.map(episode => {
+      let values = Object.values(episode).filter(value => value.includes(this.state.trackTitle))
+      // console.log("values", values)
+      console.log(values.find(value => {value.includes("Schrab")}))
+    })
+    // console.log(arr)
   }
 
   findEpisode = (e) => {
     e.preventDefault()
+    console.log(e)
   }
 
   onChange = (e) => {
     this.setState({[e.target.name]: e.target.value })
+    this.search()
   }
 
   render() {
     return (
       <Consumer>
         {value => {
+          heresTheList = value.masterList
           return (
             <div className="card card-body mb-4 p-4">
               <h1 className="display-4 text-center">
                 <i className="fas fa-microphone"></i> Search Bar
               </h1>
               <p className="lead text-center">Search for an episode</p>
-              <form onSubmit={this.findEpisode}>
+              <form>
                 <div className="form-group">
                   <input 
                     type="text" 
@@ -35,9 +50,9 @@ class Search extends Component {
                     onChange={this.onChange}
                   />
                 </div>
-                <button className="btn btn-primary btn-lg btn-block mb-5" type="submit">
+                {/* <button className="btn btn-primary btn-lg btn-block mb-5" type="submit">
                   Search
-                </button>
+                </button> */}
               </form>
             </div>
           )
